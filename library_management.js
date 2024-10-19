13d373852805425e5617bcdf992bd4a0a86819b9
Myia Lamy <mlamy@usf.edu>
2024-10-19 14:17:51 -0400

    Create Section class to manage books and availability and Calculate total available books in the section

--- library_management.js
+++ library_management.js
@@ -18,3 +18,25 @@ class Book { // Define Book class
     }
 }
 
+//Task 2: Create a Section Class
+
+class Section { // Define Section class
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) { // Used to add a book in the books array
+        this.books.push(book);
+    }
+    getAvailableBooks() { // Used to return the total number of available books in the section
+        return this.books.filter(book => book.isAvailable).length;
+    }
+    listBooks() { // Used to list all books in the section, showing their title and availability
+        return this.books.map(book => `${book.title} - ${book.isAvailable ? 'Available' : 'Borrowed'}`).join('\n');
+    }
+    //Task 5
+    calculateTotalBooksAvailable() { // Used to calculate the total number of books that are available for borrowing in the section
+        return this.books.reduce((total, book) => total + (book.isAvailable ? 1 : 0), 0);
+    }
+}
